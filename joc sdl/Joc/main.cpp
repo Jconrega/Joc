c587ec91c77547f88c6b5528365200813d53a2e5
Jconrega <xema11.1.96@gmail.com>
2015-03-04 12:03:48 +0100

    dll okay

--- joc sdl/Joc/main.cpp
+++ joc sdl/Joc/main.cpp
@@ -1,4 +1,9 @@
-#include "SDL2-2.0.3\include\SDL.h"
+#include <stdlib.h>
+
+#include "SDL2-2.0.3/include/SDL.h"
+
+#pragma comment (lib, "SDL2-2.0.3/lib/x86/SDL2.lib")
+#pragma comment (lib, "SDL2-2.0.3/lib/x86/SDL2main.lib")
 int main(int argc, char** argv)
 {
 	return 0;
